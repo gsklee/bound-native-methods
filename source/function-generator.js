fd29563003e6a3da44e9f78efe5b91004d6f785a
G. Kay Lee <balancetraveller+github@gmail.com>
2015-06-06 00:07:18 +0800

    Allow `this` to be spread only for static methods on Math

--- source/function-generator.js
+++ source/function-generator.js
@@ -1,8 +1,8 @@
 export default (object, names, rename = {}) =>
   names.reduce((m, name) => {
     m[rename[name] || name] = function (...s) {
-      return this.constructor === Array ? object[name](...this, ...s) :
-                                          object[name](this, ...s);
+      return this.constructor === Array && object === Math ? object[name](...this, ...s) :
+                                                             object[name](this, ...s);
     };
 
     return m;
